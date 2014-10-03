0da5dc93eba66266701f36caead0b2ae830ef33c
bens-unit01 <bensalem.messaoud@gmail.com>
2014-10-03 09:46:36 -0400

    new formula

--- BattleCars.ino
+++ BattleCars.ino
@@ -3,7 +3,7 @@
 
 
 #include <Servo.h>
-//#include <SoftwareSerial.h>
+#include <SoftwareSerial.h>
 
 
 #define rotation_servo_pin 3
@@ -15,7 +15,7 @@ int b, i =0;
 int angle = 90;
 
 int estimated = 90;
-//SoftwareSerial mySerial(8, 9); // RX, TX
+SoftwareSerial mySerial(8, 6); // RX, TX
 void setup()
 {
   
@@ -37,7 +37,7 @@ void setup()
   
  
   Serial.begin(9600);
-//  mySerial.begin(9600);
+  mySerial.begin(9600);
  
  delay(30);
  Serial.write(0x3e);
@@ -56,12 +56,14 @@ void setup()
 
 void loop()
 {
+  
+ 
   i++;
    if( i % 10 == 0 ) {
 
       angle = estimated;
     } else {
-     
+     if( angle != estimated){
           if( angle > estimated){
               angle--;
           } else {
@@ -70,39 +72,34 @@ void loop()
           
        if( angle < 5 ) angle = 10;
        if( angle > 170) angle = 165;
-          rotationServo.write(angle);
+       //   rotationServo.write(angle);
+       mySerial.println(angle);
           delay(15);
-        
+     } 
     }
- //    mySerial.println(estimated);
+//     mySerial.println(estimated);
     
-   if( Serial.available() >= 3 ){
-     int j = 0;
-    while(Serial.available()){
-       buffer[j] = Serial.read();
-       j++;
-        }
-        j = 0;
-      while(buffer[i] != 0x78 )  j++;
-	 //if( i > 3 ) return; 
-         // byte fwd_bwd= buffer[i+1];
-          b = buffer[j+2];  
-          int curAngle = 0;
+   if( Serial.available() > 0 ){
+     b = Serial.read();
+ 
+       int temp = 0;
        if( b > 0x60 ){
 
             //angle ++;
-            curAngle = 90 - 2*(b - 0x60 );
+            temp = 90 + 90 *( b - 0x60 ) / 32;
     
        } else {
   
-          curAngle = 90 + 2*( b - 0x40 );
+           temp = 90 - 90 *( b - 0x40 ) / 32;
+          
                 
        }
        
-       if( curAngle < 5 ) curAngle = 10;
-       if( curAngle > 170) curAngle = 165;
+       if( temp < 5 ) temp = 5;
+       if( temp > 170) temp = 170;
        
-       estimated = curAngle;
+       estimated = temp;
+    //   mySerial.print(estimated);  mySerial.print(" ");  mySerial.println(b); 
       
      
    
@@ -110,11 +107,5 @@ void loop()
   
 }
 
-void serialEvent() {
-  
-  
- 
 
- 
-}
 
